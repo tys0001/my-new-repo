3d83b2c0ce6e5aac6e621acec0482e86b8fa833d
my-new-repo <wangyu0011@gmail.com>
2026-03-15 22:37:00 +0800

    Support Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/my-new-repo/",
+export default defineConfig(() => ({
+  base: process.env.VERCEL ? "/" : "/my-new-repo/",
   plugins: [react()],
-});
+}));
